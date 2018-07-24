79c73caba2d49e4ab98e5022f60250f6f2fa0aa8
bimabima123 <bima.natawi@gmail.com>
2018-07-24 11:51:40 +0700

    update

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,16 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', 
+    { title: 'Express'}
+  );
 });
+
+router.get('/baju-celana', function(req,res,next) {
+  res.render('baju',
+    {title: "Halaman Baju"}
+);  
+});
+
 
 module.exports = router;
